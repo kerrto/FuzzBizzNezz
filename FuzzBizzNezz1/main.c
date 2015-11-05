a5af8ea93ef6e7eff70f6977be4e6fdac0fc7621
Kerry Toonen <kerrytoonen@neva-bassingthwaites-macbook-pro.local>
2015-11-05 21:35:55 +0900

    Initial Commit

---
+++ FuzzBizzNezz1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  FuzzBizzNezz1
+//
+//  Created by Kerry Toonen on 2015-11-05.
+//  Copyright © 2015 Kerrto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
